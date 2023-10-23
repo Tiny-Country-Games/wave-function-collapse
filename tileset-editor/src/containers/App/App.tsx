18ac8cacdc9fffd525193ee09341dadcf51e96ae
Jose Rodriguez Rivas <joserivas823@gmail.com>
2023-10-23 11:09:00 -0700

    Create react app

---
+++ tileset-editor/src/containers/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Container, Row} from "react-bootstrap";
+
+function App() {
+    return (
+        <Container>
+            <Row>
+
+            </Row>
+        </Container>
+    );
+}
+
+export default App;
